0f8430afcf22d9c777e44fc759dc5db934eed568
Christopher Bill <christopher.bill@einsa.net>
2019-05-24 21:06:55 +0200

    header style

--- src/component/layouts/BaseLayout.tsx
+++ src/component/layouts/BaseLayout.tsx
@@ -1,9 +1,25 @@
 import React, { FunctionComponent, memo } from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import { Navbar } from './navigation/Navbar';
 import { ConnectedUserNavigation } from './navigation/ConnectedUserNavigation';
 import { CategoryModel } from '../../model';
 import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles(() => ({
+    header: {
+        minWidth: '100%',
+        height: 200,
+        '& > h5': {
+            textAlign: 'right',
+            background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.6) 0%,rgba(255,255,255,0) 100%)',
+            padding: '0.7em',
+            color: '#fff',
+            fontSize: '22pt',
+            fontWeight: 'bold',
+        }
+    }
+}));
 
 export interface BaseLayoutStateProps {
     categories?: CategoryModel[];
@@ -14,11 +30,13 @@ export interface BaseLayoutProps extends BaseLayoutStateProps {
 }
 
 export const BaseLayout: FunctionComponent<BaseLayoutProps> = memo(({ children, sidebar, categories }) => {
+    const styles = useStyles();
     return (
         <Grid container direction={'column'} justify={'flex-start'}>
-            <Grid component={'header'} style={{ maxWidth: '100%' }}>
-                <Typography variant="h4" gutterBottom>Christian-Gottfried-Ehrenberg-Gymnasium Delitzsch
+            <Grid component={'header'} className={styles.header} style={{ backgroundImage: 'url(https://placeimg.com/1000/200/any)', backgroundSize:'cover' }}>
+                <Typography variant="h5" gutterBottom>Christian-Gottfried-Ehrenberg-Gymnasium Delitzsch
                 </Typography>
+                <div  />
             </Grid>
             <Navbar categories={categories} />
             <Grid item style={{ marginTop: '.5em', maxWidth: '100%' }}>
