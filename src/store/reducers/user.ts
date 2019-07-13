5099ecc2ee8f2f96b4bcc0553daad79def995e0b
Alexis Rinaldoni <arinaldoni@me.com>
2019-07-14 00:19:01 +0200

    keep user logged in

--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -1,11 +1,28 @@
 import { UserActionType, LoginAction, LogoutAction } from '../actions/user';
 import { UserState } from '../State';
-import { mockData } from '../../mockData';
-import { set, remove } from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+import { get, set, remove } from 'js-cookie';
+import { UserModel } from 'model';
 
 export type UserActions = LoginAction | LogoutAction;
 
-export const initialUserState: UserState = mockData.user;
+export const initialUserState: UserState = {
+    user: (() => {
+        const token = get(process.env.REACT_APP_AUTHENTICATION_TOKEN_NAME);
+        if (token) {
+            const decoded: any = jwtDecode(token);
+            if (decoded.exp * 1000 > new Date().getTime()) {
+                return {
+                    id: decoded.sub,
+                    name: decoded.name,
+                    email: decoded.email,
+
+                } as UserModel;
+            }
+        }
+        return null;
+    })()
+};
 
 export const userReducer = (s: UserState = initialUserState, action: UserActions): UserState => {
     switch (action.type) {
