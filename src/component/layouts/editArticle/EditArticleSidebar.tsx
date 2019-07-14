f8eef1b274a996a15cd3d8a3486a4d8f4e1ef252
Alexis Rinaldoni <arinaldoni@me.com>
2019-07-14 21:38:08 +0200

    fix warnings

--- src/component/layouts/editArticle/EditArticleSidebar.tsx
+++ src/component/layouts/editArticle/EditArticleSidebar.tsx
@@ -1,5 +1,5 @@
 import React, { memo, FunctionComponent } from 'react';
-import { Card, CardContent, CardMedia, Divider, TextField, Button, Typography, makeStyles } from '@material-ui/core';
+import { Card, CardContent, Divider, TextField, Button, makeStyles } from '@material-ui/core';
 import { ArticleModel } from '../../../model';
 import clsx from 'clsx';
 import { Save as SaveIcon, Edit } from '@material-ui/icons';
@@ -23,110 +23,111 @@ interface EditArticleSidebarProps {
 }
 
 export const EditArticleSidebar: FunctionComponent<EditArticleSidebarProps> = memo(({ article, onUpdate, onSave }) => {
-        const styles = useStyles();
-        return (
-    <Card style={{marginTop: '0.5em'}}>
-        <CardContent>
-            <TextField
-                label="Titel des Beitrags"
-                placeholder="Placeholder"
-                value={article.title}
-                onChange={e => onUpdate({ ...article, title: e.target.value })}
-                fullWidth
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-        </CardContent>
-        <Divider />
-        <CardContent>
-            <TextField
-                label="Vorschautext"
-                placeholder="Füge hier einen kurzen Vorschautext ein"
-                value={article.preview}
-                onChange={e => onUpdate({ ...article, preview: e.target.value })}
-                fullWidth
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-        </CardContent>
-        <Divider />
-        <CardContent>
-            <TextField
-                label="Datum"
-                type={'date'}
-                value={article.updatedAt}
-                disabled
-                fullWidth
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-        </CardContent>
-        <Divider />
-        <CardContent>
-            <TextField
-                label="Kategorie"
-                fullWidth
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-        </CardContent>
-        <CardContent>
-            <TextField
-                label="Seite"
-                value={article.pageName}
-                onChange={e => onUpdate({ ...article, pageName: e.target.value || undefined })}
-                fullWidth
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-        </CardContent>
-        <Divider />
-        <CardContent>
-            <img src={'https://placeimg.com/300/150/any'} style={{ width: '100%', height: 'auto' }} title={`Vorschaubild zu ${article.title}`} />
-            <Button
-            variant='outlined'
-            color='secondary'
-            >
-                <Edit className={clsx(styles.leftIcon, styles.iconSmall)} />
-                Vorschaubild ändern
+    const styles = useStyles();
+    return (
+        <Card style={{ marginTop: '0.5em' }}>
+            <CardContent>
+                <TextField
+                    label="Titel des Beitrags"
+                    placeholder="Placeholder"
+                    value={article.title}
+                    onChange={e => onUpdate({ ...article, title: e.target.value })}
+                    fullWidth
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </CardContent>
+            <Divider />
+            <CardContent>
+                <TextField
+                    label="Vorschautext"
+                    placeholder="Füge hier einen kurzen Vorschautext ein"
+                    value={article.preview}
+                    onChange={e => onUpdate({ ...article, preview: e.target.value })}
+                    fullWidth
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </CardContent>
+            <Divider />
+            <CardContent>
+                <TextField
+                    label="Datum"
+                    type={'date'}
+                    value={article.updatedAt}
+                    disabled
+                    fullWidth
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </CardContent>
+            <Divider />
+            <CardContent>
+                <TextField
+                    label="Kategorie"
+                    fullWidth
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </CardContent>
+            <CardContent>
+                <TextField
+                    label="Seite"
+                    value={article.pageName}
+                    onChange={e => onUpdate({ ...article, pageName: e.target.value || undefined })}
+                    fullWidth
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </CardContent>
+            <Divider />
+            <CardContent>
+                <img src={'https://placeimg.com/300/150/any'} style={{ width: '100%', height: 'auto' }} alt={`Vorschaubild zu ${article.title}`} />
+                <Button
+                    variant='outlined'
+                    color='secondary'
+                >
+                    <Edit className={clsx(styles.leftIcon, styles.iconSmall)} />
+                    Vorschaubild ändern
             </Button>
-        </CardContent>
-        <Divider />
-        <CardContent>
-            <TextField
-                label="Autoren"
-                fullWidth
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-        </CardContent>
-        <Divider />
-        <CardContent>
-            <TextField
-                label="Sichtbarkeit"
-                fullWidth
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-        </CardContent>
-        <Divider />
-        <CardContent>
-            <Button 
-            onClick={onSave}
-            variant='outlined'
-            color='secondary'
-            >
-                <SaveIcon className={clsx(styles.leftIcon, styles.iconSmall)} />
-                speichern
+            </CardContent>
+            <Divider />
+            <CardContent>
+                <TextField
+                    label="Autoren"
+                    fullWidth
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </CardContent>
+            <Divider />
+            <CardContent>
+                <TextField
+                    label="Sichtbarkeit"
+                    fullWidth
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </CardContent>
+            <Divider />
+            <CardContent>
+                <Button
+                    onClick={onSave}
+                    variant='outlined'
+                    color='secondary'
+                >
+                    <SaveIcon className={clsx(styles.leftIcon, styles.iconSmall)} />
+                    speichern
             </Button>
-        </CardContent>
-    </Card>
-)}
+            </CardContent>
+        </Card>
+    )
+}
 );
