3e46dc5fb318b94b7c41af86f212163e52329e49
Christopher Bill <christopher.bill@einsa.net>
2019-07-20 17:17:03 +0200

    style site-headline

--- src/component/layouts/PageLayout.tsx
+++ src/component/layouts/PageLayout.tsx
@@ -1,9 +1,22 @@
 import React, { FunctionComponent, memo } from 'react';
 import { Article } from '../article/Article';
 import { ArticleModel } from '../../model';
-import { Typography } from '@material-ui/core';
+import { Typography, makeStyles, Theme } from '@material-ui/core';
 import { BaseLayoutMainContent } from './BaseLayoutMainContent';
 import { BaseLayoutSidebar } from './BaseLayoutSidebar';
+
+const useStyle = makeStyles((theme: Theme) => ({
+    siteTitle: {
+        paddingTop: '1.5em', 
+        paddingBottom: '1em', 
+        paddingLeft: '1em', 
+        fontSize: '1.5rem',
+        letterSpacing: '0.3em', 
+        color: theme.palette.primary.main, 
+        backgroundColor: '#fff',
+        textTransform: 'uppercase',
+    }
+}));
 
 export interface PageLayoutProps {
     title?: string;
@@ -11,10 +24,13 @@ export interface PageLayoutProps {
 }
 
 export const PageLayout: FunctionComponent<PageLayoutProps> = memo(({ articles, title }) => {
+
+    const styles = useStyle();
+
     return (
         <>
             <BaseLayoutMainContent>
-                {title && <Typography variant={'h3'} style={{ paddingTop: '1em', paddingBottom: '1em', paddingLeft: '0.4em', fontSize: '2.5rem', letterSpacing: '0.2em', color: '#9f9f9f', backgroundColor: '#fff' }}>{title}</Typography>}
+                {title && <Typography variant={'h3'} className={styles.siteTitle} >{title}</Typography>}
                 {articles && articles.map(article => (
                     <Article key={article.id} article={article} />
                 ))}
