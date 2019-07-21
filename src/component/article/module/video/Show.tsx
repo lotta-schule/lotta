927a8e181cc78f5afefbe37b539bb78130196a5e
Alexis Rinaldoni <arinaldoni@me.com>
2019-07-21 16:32:19 +0200

    show different placeholder for image conversion

--- src/component/article/module/video/Show.tsx
+++ src/component/article/module/video/Show.tsx
@@ -9,20 +9,29 @@ interface ShowProps {
 }
 
 export const Show: FunctionComponent<ShowProps> = memo(({ contentModule }) => {
-    const videoFiles = contentModule.files && contentModule.files.length > 0 && contentModule.files[0].fileConversions &&
+    const file = contentModule.files && contentModule.files.length > 0 && contentModule.files[0];
+    const videoFiles = file && file.fileConversions &&
         contentModule.files[0].fileConversions.filter(f => /^video/.test(f.mimeType));
     const posterFile = contentModule.files && contentModule.files.length > 0 && contentModule.files[0].fileConversions &&
         contentModule.files[0].fileConversions.length > 0 && find(contentModule.files[0].fileConversions, fc => /^storyboard/.test(fc.format));
     const posterFileLocation = posterFile && posterFile.remoteLocation;
-    return (videoFiles && videoFiles.length ? (
+    if (!file) {
+        return (
+            <PlaceholderImage width={'100%'} height={350} />
+        );
+    }
+    if (!videoFiles || !videoFiles.length) {
+        return (
+            <PlaceholderImage width={'100%'} height={350} icon={'video'} />
+        );
+    }
+    return (
         <Player
             playsInline
             poster={posterFileLocation || undefined}
         >
-            {videoFiles.map(vf => <source src={vf.remoteLocation} type={vf.mimeType} />)}
+            {videoFiles.map(vf => <source key={vf.remoteLocation} src={vf.remoteLocation} type={vf.mimeType} />)}
             <ControlBar autoHide={true} />
         </Player>
-    ) : (
-            <PlaceholderImage width={'100%'} height={350} />
-        ));
+    );
 });
