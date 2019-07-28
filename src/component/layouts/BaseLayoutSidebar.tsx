5c107f29ef30fbac28ec7e86f46856d8c7ae8195
Christopher Bill <christopher.bill@einsa.net>
2019-07-28 16:37:58 +0200

    breakpoints for margins: aside / user-navigation

--- src/component/layouts/BaseLayoutSidebar.tsx
+++ src/component/layouts/BaseLayoutSidebar.tsx
@@ -1,10 +1,25 @@
 import React, { FunctionComponent, memo } from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import { UserNavigation } from './navigation/UserNavigation';
 
-export const BaseLayoutSidebar: FunctionComponent = memo(({ children }) => (
-    <Grid item component={'aside'} xs={12} md={3} xl={3} style={{ marginLeft: '0.5em' }}>
+const useStyles = makeStyles(theme => ({
+    root: {
+        [theme.breakpoints.down('md')]: {
+            marginLeft: 0,
+            marginTop: '0.5em',
+          },
+        [theme.breakpoints.up('md')]: {
+            marginLeft: '0.5em',
+        },
+    },
+}));
+
+export const BaseLayoutSidebar: FunctionComponent = memo(({ children }) => {
+        const styles = useStyles();
+
+    return (
+    <Grid className={styles.root} item component={'aside'} xs={12} md={3} xl={3} >
         <UserNavigation />
         {children}
     </Grid>
-));
+)});
