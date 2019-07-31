2a1cd96e6894651bc1c41417822532bd5dcea300
Alexis Rinaldoni <arinaldoni@me.com>
2019-07-31 22:10:58 +0200

    add lists to editor, restyle editor a bit (groupedButtons)

--- src/component/article/module/text/Show.tsx
+++ src/component/article/module/text/Show.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, memo } from 'react';
 import { ContentModuleModel } from '../../../../model';
 import { Editor } from 'slate-react';
-import { renderMark } from './SlateUtils';
+import { renderMark, plugins, renderBlock } from './SlateUtils';
 const { deserialize } = require('slate-base64-serializer').default;
 
 interface ShowProps {
@@ -12,8 +12,10 @@ export const Show: FunctionComponent<ShowProps> = memo(({ contentModule }) => {
 
     return (
         <Editor
+            plugins={plugins}
             value={deserialize(contentModule.text)}
             renderMark={renderMark}
+            renderBlock={renderBlock}
             readOnly
         />
     );
