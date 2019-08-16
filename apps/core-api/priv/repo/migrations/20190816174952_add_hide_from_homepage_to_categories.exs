beb11dd30e0cb3cd46b0c6a37c6e262453b2adb5
Alexis Rinaldoni <arinaldoni@me.com>
2019-08-16 22:30:43 +0200

    add hide articles from homepage

---
+++ apps/core-api/priv/repo/migrations/20190816174952_add_hide_from_homepage_to_categories.exs
@@ -0,0 +1,9 @@
+defmodule Api.Repo.Migrations.AddHideFromHomepageToCategories do
+  use Ecto.Migration
+
+  def change do
+    alter table(:categories) do
+      add :hide_articles_from_homepage, :boolean, null: false, default: false
+    end
+  end
+end
