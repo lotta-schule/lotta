c333d3cd08b3aaf5e46f42519e69c1e75698d7e0
Alexis Rinaldoni <arinaldoni@me.com>
2019-08-25 19:40:19 +0200

    use apollo cache for getArticle

--- src/component/routes/ArticleRoute.tsx
+++ src/component/routes/ArticleRoute.tsx
@@ -2,47 +2,31 @@ import React, { memo } from 'react';
 import { ArticleLayout } from 'component/layouts/ArticleLayout';
 import { ArticleModel } from 'model';
 import { CircularProgress } from '@material-ui/core';
-import { createAddArticleAction } from 'store/actions/content';
-import { find } from 'lodash';
 import { GetArticleQuery } from 'api/query/GetArticleQuery';
 import { ID } from 'model/ID';
-import { Query } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 import { RouteComponentProps } from 'react-router-dom';
-import { State } from 'store/State';
-import { useSelector, useDispatch } from 'react-redux';
 
 export const ArticleRoute = memo<RouteComponentProps<{ id: string }>>(({ match }) => {
     const id = Number(match.params.id);
-    const article = useSelector<State, ArticleModel | undefined>(state => find(state.content.articles, { id }));
-    const dispatch = useDispatch();
-    if (article) {
+    const { data, loading: isLoading, error } = useQuery<{ article: ArticleModel }, { id: ID }>(GetArticleQuery, { variables: { id } });
+
+    if (isLoading) {
+        return <div><CircularProgress /></div>;
+    }
+    if (error) {
+        return (<div><span style={{ color: 'red' }}>{error.message}</span></div>);
+    }
+
+    if (data && data.article) {
         return (
             <ArticleLayout
                 title={''}
-                article={article}
+                article={data.article}
             />
         );
     }
     return (
-        <Query<{ article: ArticleModel }, { id: ID }> query={GetArticleQuery} variables={{ id }}>
-            {({ data, loading: isLoading, error }) => {
-                if (!data || isLoading) {
-                    return <div><CircularProgress /></div>;
-                }
-                if (error) {
-                    return (<div><span style={{ color: 'red' }}>{error.message}</span></div>);
-                }
-                if (data) {
-                    dispatch(createAddArticleAction(data.article))
-                    return (
-                        <ArticleLayout
-                            title={''}
-                            article={data.article}
-                        />
-                    );
-                }
-                return null;
-            }}
-        </Query>
+        <span style={{ color: 'red' }}>Keine Daten</span>
     );
 });
