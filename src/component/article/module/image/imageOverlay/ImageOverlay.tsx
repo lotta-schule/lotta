a3691b6c5fb20b1873d6d4eb0c1aa19b268d31d4
Alexis Rinaldoni <arinaldoni@me.com>
2019-09-18 19:03:53 +0200

    make galeries navigable

--- src/component/article/module/image/imageOverlay/ImageOverlay.tsx
+++ src/component/article/module/image/imageOverlay/ImageOverlay.tsx
@@ -1,7 +1,7 @@
-import React, { FunctionComponent, memo, MouseEvent } from 'react';
+import React, { FunctionComponent, memo, MouseEvent, useEffect, KeyboardEventHandler, KeyboardEvent, useCallback } from 'react';
 import { FileModel } from 'model';
 import { makeStyles } from '@material-ui/styles';
-import { Theme, Button, Fab, IconButton } from '@material-ui/core';
+import { Theme, IconButton } from '@material-ui/core';
 import { useWindowSize } from 'util/useWindowSize';
 import { useLockBodyScroll } from 'util/useLockBodyScroll';
 import { Close, ChevronLeft, ChevronRight } from '@material-ui/icons';
@@ -19,38 +19,54 @@ const useStyles = makeStyles((theme: Theme) => ({
         zIndex: 100000,
         backgroundColor: '#fff',
     },
-    image: {
-        width: '80vw',
-        height: '80vh',
-    },
     closeButton: {
         position: 'absolute',
-        top: '.25em',
-        right: '3%',
+        top: theme.spacing(1),
+        right: theme.spacing(1),
         '&:hover': {
             backgroundColor: theme.palette.secondary.light,
         }
     },
     leftButton: {
         position: 'absolute',
-        left: '3%',
+        left: theme.spacing(1),
     },
     rightButton: {
         position: 'absolute',
-        right: '3%',
+        right: theme.spacing(1),
     }
 }));
 
 export interface ImageOverlayProps {
     selectedFile: FileModel | null;
-    onClose(e: MouseEvent<HTMLButtonElement>): void;
+    onPrevious?(e: MouseEvent<HTMLButtonElement> | KeyboardEvent<Window>): void;
+    onNext?(e: MouseEvent<HTMLButtonElement> | KeyboardEvent<Window>): void;
+    onClose(e: MouseEvent<HTMLButtonElement> | KeyboardEvent<Window>): void;
 }
 
-export const ImageOverlay: FunctionComponent<ImageOverlayProps> = memo(({ selectedFile, onClose }) => {
+export const ImageOverlay: FunctionComponent<ImageOverlayProps> = memo(({ selectedFile, onPrevious, onNext, onClose }) => {
     useLockBodyScroll();
     const styles = useStyles();
     const { innerHeight, innerWidth } = useWindowSize();
     const [width, height] = [Math.floor(innerWidth * .8), Math.floor(innerHeight * .8)];
+
+    const onKeyDown: KeyboardEventHandler<Window> = useCallback(event => {
+        if (event.keyCode === 27) { // ESC
+            onClose(event);
+        } else if (event.keyCode === 37 && onPrevious) { // <-
+            onPrevious(event);
+        } else if (event.keyCode === 39 && onNext) { // ->
+            onNext(event);
+        }
+    }, [onClose, onNext, onPrevious]);
+
+    useEffect(() => {
+        window.addEventListener('keydown', onKeyDown as any);
+        return () => {
+            window.removeEventListener('keydown', onKeyDown as any);
+        };
+
+    }, [onKeyDown]);
 
     if (!selectedFile) {
         return null;
@@ -58,16 +74,20 @@ export const ImageOverlay: FunctionComponent<ImageOverlayProps> = memo(({ select
     const imgUrl = `https://afdptjdxen.cloudimg.io/bound/${width}x${height}/foil1/${selectedFile.remoteLocation}`;
     return (
         <div className={styles.root}>
-            <IconButton size="medium" color="secondary" className={styles.closeButton} onClick={onClose}>
+            <IconButton size="medium" color={'secondary'} className={styles.closeButton} onClick={onClose}>
                 <Close />
             </IconButton>
-            <IconButton size="small" color="secondary" className={styles.leftButton}>
-                <ChevronLeft />
-            </IconButton>
-            <IconButton size="small" color="secondary" className={styles.rightButton}>
-                <ChevronRight />
-            </IconButton>
-            <img src={imgUrl} alt={''} style={{ width: '75%' }} />
+            {onPrevious && (
+                <IconButton size={'small'} color={'secondary'} className={styles.leftButton} onClick={onPrevious}>
+                    <ChevronLeft />
+                </IconButton>
+            )}
+            {onNext && (
+                <IconButton size={'small'} color={'secondary'} className={styles.rightButton} onClick={onNext}>
+                    <ChevronRight />
+                </IconButton>
+            )}
+            <img src={imgUrl} alt={''} />
         </div>
     );
 });
