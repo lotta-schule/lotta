b7b98d9b43fde6d9539efa1acab05db426ae48d5
Alexis Rinaldoni <arinaldoni@me.com>
2019-09-20 15:51:23 +0200

    fix queries to get avatarImage

--- src/api/query/GetUsersQuery.ts
+++ src/api/query/GetUsersQuery.ts
@@ -18,6 +18,9 @@ export const GetUsersQuery = gql`
                     id
                 }
             }
+            avatarImageFile {
+                remoteLocation
+            }
         }
     }
 `;
