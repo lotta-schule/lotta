65bee465deb3cc72f2e3b9da2babe1a7a7e4603f
Alexis Rinaldoni <arinaldoni@me.com>
2019-10-09 10:12:37 +0200

    Revert "gitlab - shared runner: set correct db/rabbitmq host"
    This reverts commit 0e16838e9dbfb9acbda17cee9a0b52676fb2bce0.

--- apps/core-api/config/test.exs
+++ apps/core-api/config/test.exs
@@ -1,17 +1,26 @@
 import Config
 
+db_host = case System.get_env("CI") do
+  nil -> "postgres"
+  _ -> "127.0.0.1"
+end
+rabbitmq_host = case System.get_env("CI") do
+  nil -> "rabbitmq"
+  _ -> "127.0.0.1"
+end
+
 # Configure your database
 config :api, Api.Repo,
   username: "lotta",
   password: "lotta",
   database: "api_test",
-  hostname: "postgres",
+  hostname: db_host,
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :api, :rabbitmq_connection,
   username: "guest",
   password: "guest",
-  host: "rabbitmq"
+  host: rabbitmq_host
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
