330d8b313a5208aac6ad8801bd6886cbb5f9c753
Alexis Rinaldoni <arinaldoni@me.com>
2019-10-24 19:04:19 +0200

    category: fetch more articles on scroll

--- src/component/routes/CategoryRoute.tsx
+++ src/component/routes/CategoryRoute.tsx
@@ -1,20 +1,52 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { CategoryLayout } from 'component/layouts/CategoryLayout';
 import { useCategory } from 'util/categories/useCategory';
 import { useQuery } from 'react-apollo';
-import { ArticleModel } from 'model';
+import { ArticleModel, ArticleFilter } from 'model';
 import { GetArticlesQuery } from 'api/query/GetArticlesQuery';
 import { EmptyLoadingLayout } from 'component/layouts/EmptyLoadingLayout';
 import { ID } from 'model/ID';
+import { parseISO } from 'date-fns';
 
 export const CategoryRoute = memo<RouteComponentProps<{ id: string }>>(({ match }) => {
     const categoryId = Number(match.params.id);
     const category = useCategory(categoryId);
 
-    const { data, loading: isLoading, error } = useQuery<{ articles: ArticleModel[] }, { categoryId: ID }>(GetArticlesQuery, { variables: { categoryId } });
+    const FETCH_MORE_OFFSET = 50;
 
-    if (isLoading) {
+    const { data, loading: isLoading, error, fetchMore } = useQuery<{ articles: ArticleModel[] }, { categoryId: ID, filter: ArticleFilter }>(
+        GetArticlesQuery,
+        { variables: { categoryId, filter: { first: 20 } } }
+    );
+
+    const handleScroll = useCallback(() => {
+        if (window.innerHeight + Math.max(window.pageYOffset, document.documentElement.scrollTop) > document.documentElement.offsetHeight - FETCH_MORE_OFFSET) {
+            if (data && data.articles && data.articles.length && !isLoading) {
+                console.log('Fetch more list items!');
+                const lastDate = data && data.articles
+                    .sort((a1, a2) => parseISO(a1.updatedAt).getTime() - parseISO(a2.updatedAt).getTime())[0].updatedAt;
+                console.log(lastDate);
+                fetchMore({
+                    variables: { filter: { first: 20, updated_before: lastDate } },
+                    updateQuery: (prev: { articles: ArticleModel[] }, { fetchMoreResult }) => {
+                        if (!fetchMoreResult) return prev;
+                        return {
+                            ...prev,
+                            articles: [...data.articles, ...fetchMoreResult.articles]
+                        };
+                    }
+                });
+            }
+        }
+    }, [data, fetchMore, isLoading]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [handleScroll]);
+
+    if (!data && isLoading) {
         return (
             <EmptyLoadingLayout />
         );
@@ -37,10 +69,13 @@ export const CategoryRoute = memo<RouteComponentProps<{ id: string }>>(({ match 
             data.articles.filter(a => Boolean(a.category && !a.category.hideArticlesFromHomepage)) :
             data.articles;
         return (
-            <CategoryLayout
-                category={category}
-                articles={articles}
-            />
+            <>
+                <CategoryLayout
+                    category={category}
+                    articles={articles}
+                />
+                {isLoading && <EmptyLoadingLayout />}
+            </>
         )
     }
 
