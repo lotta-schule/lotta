128d9f85de7f846dfba54d74d321181cea490735
Alexis Rinaldoni <arinaldoni@me.com>
2019-10-25 07:06:47 +0200

    do not load more articles if no more come, add loading indicator, add FETCH_MORE

--- src/component/routes/CategoryRoute.tsx
+++ src/component/routes/CategoryRoute.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useCallback, useEffect } from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { CategoryLayout } from 'component/layouts/CategoryLayout';
 import { useCategory } from 'util/categories/useCategory';
@@ -12,34 +12,45 @@ import { parseISO } from 'date-fns';
 export const CategoryRoute = memo<RouteComponentProps<{ id: string }>>(({ match }) => {
     const categoryId = Number(match.params.id);
     const category = useCategory(categoryId);
+    const [lastFetchedElementDate, setLastFetchedElementDate] = useState<string | null>(null);
 
     const FETCH_MORE_OFFSET = 50;
+    const FETCH_COUNT = 25;
 
     const { data, loading: isLoading, error, fetchMore } = useQuery<{ articles: ArticleModel[] }, { categoryId: ID, filter: ArticleFilter }>(
         GetArticlesQuery,
-        { variables: { categoryId, filter: { first: 20 } } }
+        {
+            variables: { categoryId, filter: { first: FETCH_COUNT } },
+            notifyOnNetworkStatusChange: true
+        },
     );
 
     const handleScroll = useCallback(() => {
         if (window.innerHeight + Math.max(window.pageYOffset, document.documentElement.scrollTop) > document.documentElement.offsetHeight - FETCH_MORE_OFFSET) {
-            if (data && data.articles && data.articles.length && !isLoading) {
-                console.log('Fetch more list items!');
+            if (data && data.articles && data.articles.length > FETCH_COUNT - 1 && !isLoading) {
                 const lastDate = data && data.articles
                     .sort((a1, a2) => parseISO(a1.updatedAt).getTime() - parseISO(a2.updatedAt).getTime())[0].updatedAt;
-                console.log(lastDate);
-                fetchMore({
-                    variables: { filter: { first: 20, updated_before: lastDate } },
-                    updateQuery: (prev: { articles: ArticleModel[] }, { fetchMoreResult }) => {
-                        if (!fetchMoreResult) return prev;
-                        return {
-                            ...prev,
-                            articles: [...data.articles, ...fetchMoreResult.articles]
-                        };
-                    }
-                });
+                if (lastFetchedElementDate !== lastDate) {
+                    fetchMore({
+                        variables: { filter: { first: FETCH_COUNT, updated_before: lastDate } },
+                        updateQuery: (prev: { articles: ArticleModel[] }, { fetchMoreResult }) => {
+                            if (!fetchMoreResult) return prev;
+                            const fetchedArticles = fetchMoreResult.articles;
+                            if (fetchedArticles && fetchedArticles.length) {
+                                const lastDate = fetchedArticles[fetchedArticles.length - 1].updatedAt;
+                                setLastFetchedElementDate(lastDate);
+                            }
+                            return {
+                                ...prev,
+                                articles: [...data.articles, ...fetchMoreResult.articles]
+                            };
+                        },
+
+                    });
+                }
             }
         }
-    }, [data, fetchMore, isLoading]);
+    }, [data, fetchMore, isLoading, lastFetchedElementDate]);
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
