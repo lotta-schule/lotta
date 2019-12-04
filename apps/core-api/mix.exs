18af5af1747f53fe58c39ece36086ffacb436329
Alexis Rinaldoni <arinaldoni@me.com>
2019-12-04 18:04:43 +0100

    1.1.0

--- apps/core-api/mix.exs
+++ apps/core-api/mix.exs
@@ -4,7 +4,7 @@ defmodule Api.MixProject do
   def project do
     [
       app: :api,
-      version: "0.1.0",
+      version: "1.1.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
