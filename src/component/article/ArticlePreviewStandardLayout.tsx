37b271e3e01eff6b66c9e1e8b4b1e2faef42f7c7
Alexis Rinaldoni <arinaldoni@me.com>
2020-04-25 13:32:45 +0200

    fix mobile preview 2columns

--- src/component/article/ArticlePreviewStandardLayout.tsx
+++ src/component/article/ArticlePreviewStandardLayout.tsx
@@ -11,6 +11,7 @@ import { useMutation } from '@apollo/react-hooks';
 import { ToggleArticlePinMutation } from 'api/mutation/ToggleArticlePin';
 import { CollisionLink } from '../general/CollisionLink';
 import { AuthorAvatarsList } from './AuthorAvatarsList';
+import { useIsMobile } from 'util/useIsMobile';
 import clsx from 'clsx';
 import Img from 'react-cloudimage-responsive';
 
@@ -26,6 +27,11 @@ const useStyle = makeStyles<Theme, { isEmbedded?: boolean, narrow?: boolean }>(t
                 color: theme.palette.primary.contrastText,
                 backgroundColor: theme.palette.secondary.main,
             },
+        }
+    },
+    cardContent: {
+        [theme.breakpoints.down('sm')]: {
+            padding: theme.spacing(1, 0)
         }
     },
     editButton: {
@@ -53,7 +59,13 @@ const useStyle = makeStyles<Theme, { isEmbedded?: boolean, narrow?: boolean }>(t
         marginLeft: theme.spacing(2)
     },
     articleTitle: {
-        ...(theme.overrides && (theme.overrides as any).LottaArticlePreview && (theme.overrides as any).LottaArticlePreview.title)
+        ...(theme.overrides && (theme.overrides as any).LottaArticlePreview && (theme.overrides as any).LottaArticlePreview.title),
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '1.2rem',
+            lineHeight: 1.05,
+            wordBreak: 'break-word',
+            hyphens: 'auto'
+        }
     },
     subtitle: {
         display: ({ narrow }) => narrow ? 'block' : 'flex',
@@ -61,6 +73,13 @@ const useStyle = makeStyles<Theme, { isEmbedded?: boolean, narrow?: boolean }>(t
         textTransform: 'uppercase',
         fontSize: '0.85rem',
         marginBottom: theme.spacing(1),
+        [theme.breakpoints.down('sm')]: {
+            display: 'flex !important',
+            lineHeight: 1,
+            '& span:last-child': {
+                textAlign: 'right'
+            }
+        },
         '& span': {
             [theme.breakpoints.down('md')]: {
                 display: ({ narrow }) => narrow ? 'block' : 'initial',
@@ -75,6 +94,9 @@ const useStyle = makeStyles<Theme, { isEmbedded?: boolean, narrow?: boolean }>(t
         WebkitBoxOrient: 'vertical',
         boxOrient: 'vertical',
         display: '-webkit-box',
+        [theme.breakpoints.down('sm')]: {
+            lineHeight: 1.3
+        }
     }
 }
 ));
@@ -90,7 +112,7 @@ interface ArticlePreviewProps {
 }
 
 export const ArticlePreviewStandardLayout = memo<ArticlePreviewProps>(({ article, disableLink, disableEdit, disablePin, limitedHeight, isEmbedded, narrow }) => {
-
+    const isMobile = useIsMobile();
     const [currentUser] = useCurrentUser();
 
     const styles = useStyle({ isEmbedded, narrow });
@@ -127,37 +149,43 @@ export const ArticlePreviewStandardLayout = memo<ArticlePreviewProps>(({ article
                     </Grid>
                 )}
                 <Grid item xs>
-                    <CardContent>
+                    <CardContent className={styles.cardContent}>
                         <Typography component={'h5'} variant={'h5'} gutterBottom className={styles.articleTitle}>
                             {!isEmbedded && currentUser && currentUser.lastSeen && isBefore(new Date(currentUser.lastSeen), new Date(article.updatedAt)) && (
                                 <FiberManualRecord color={'secondary'} fontSize={'small'} />
                             )}
                             {maybeLinked(article.title)}
-                            {!disableEdit && User.canEditArticle(currentUser, article) && (
-                                <Fab
-                                    aria-label="Edit"
-                                    size="small"
-                                    className={clsx(styles.editButton, 'edit-button')}
-                                    component={CollisionLink}
-                                    to={Article.getPath(article, { edit: true })}
-                                >
-                                    <Edit />
-                                </Fab>
-                            )}
-                            {!disablePin && User.isAdmin(currentUser) && (
-                                <Fab
-                                    aria-label="Pin"
-                                    size="small"
-                                    className={clsx(styles.pinButton, { active: article.isPinnedToTop })}
-                                    onClick={() => toggleArticlePin()}
-                                >
-                                    <Place />
-                                </Fab>
+                            {(!isMobile || isEmbedded) && (
+                                <>
+                                    {!disableEdit && User.canEditArticle(currentUser, article) && (
+                                        <Fab
+                                            aria-label="Edit"
+                                            size="small"
+                                            className={clsx(styles.editButton, 'edit-button')}
+                                            component={CollisionLink}
+                                            to={Article.getPath(article, { edit: true })}
+                                        >
+                                            <Edit />
+                                        </Fab>
+                                    )}
+                                    {!disablePin && User.isAdmin(currentUser) && (
+                                        <Fab
+                                            aria-label="Pin"
+                                            size="small"
+                                            className={clsx(styles.pinButton, { active: article.isPinnedToTop })}
+                                            onClick={() => toggleArticlePin()}
+                                        >
+                                            <Place />
+                                        </Fab>
+                                    )}
+                                </>
                             )}
                         </Typography>
                         <Typography variant={'subtitle1'} className={clsx(styles.subtitle)}>
-                            <span>{format(new Date(article.updatedAt), 'PPP', { locale: de }) + ' '}</span>
-                            <span>{article.topic && <> | {article.topic}&nbsp;</>}</span>
+                            <span>{format(new Date(article.updatedAt), narrow && isMobile ? 'P' : 'PPP', { locale: de }) + ' '}</span>
+                            {!isMobile && (
+                                <span>{article.topic && <> | {article.topic}&nbsp;</>}</span>
+                            )}
                             <span>{article.users && article.users.length > 0 && <>&nbsp;<AuthorAvatarsList className={styles.authorAvatarsList} users={article.users} max={8} />&nbsp;</>}</span>
                         </Typography>
                         <Typography variant={'subtitle1'} color="textSecondary" className={clsx({ [styles.previewTextLimitedHeight]: limitedHeight })}>
