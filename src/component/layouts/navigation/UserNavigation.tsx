1bfce203e22387bae2e6b9c688f0cb62a0e46d7f
Christopher Bill <cb.billy.94@gmail.com>
2020-05-26 22:09:00 +0200

    button align in 'user navigation' for being logged out

--- src/component/layouts/navigation/UserNavigation.tsx
+++ src/component/layouts/navigation/UserNavigation.tsx
@@ -141,7 +141,7 @@ export const UserNavigation = memo(() => {
         );
     } else {
         return (
-            <Grid container justify={'flex-end'} style={{ display:'grid', height: '100%' }}>
+            <Grid container style={{ flexDirection: 'column', justifyContent: 'space-evenly', height: '100%', alignItems: 'end' }}>
                 <Button size="small" onClick={() => setLoginModalIsOpen(true)}>Anmelden</Button>
                 <Button size="small" onClick={() => setRegisterModalIsOpen(true)}>Registrieren</Button>
                 <Button size="small" onClick={() => history.push('/search')}>Suche</Button>
