b0bf283da275752dda29d2c257e626c25aafce9a
Alexis Rinaldoni <arinaldoni@me.com>
2020-07-12 05:04:58 +0200

    react to auth_error

--- apps/core-api/lib/api/guardian/auth_error_handler.ex
+++ apps/core-api/lib/api/guardian/auth_error_handler.ex
@@ -3,10 +3,17 @@ defmodule Api.Guardian.AuthErrorHandler do
     Authentication error handler for Guardian
   """
 
+  require Logger
+
+  import Plug.Conn
+
   @behaviour Guardian.Plug.ErrorHandler
   @impl Guardian.Plug.ErrorHandler
 
-  def auth_error(_conn, {_type, reason}, _opts) do
-    throw(reason)
+  def auth_error(conn, {type, reason}, _opts) do
+    Logger.error("user authentication error:#{inspect(type)} #{inspect(reason)}")
+
+    conn
+    |> send_resp(401, Jason.encode!(%{message: to_string(type)}))
   end
 end
