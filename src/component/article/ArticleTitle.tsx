dc52eb725204d8aa5387d7a47bc3eabdcd89b1bc
Christopher Bill <cb.billy.94@gmail.com>
2020-07-14 12:45:31 +0200

    layout & style header

--- src/component/article/ArticleTitle.tsx
+++ src/component/article/ArticleTitle.tsx
@@ -1,62 +1,179 @@
 import React, { memo } from 'react';
-import { Typography, makeStyles, Theme, Grid } from '@material-ui/core';
-import { ArticleModel } from 'model';
+import { Typography, makeStyles, Theme, Grid, IconButton, Container } from '@material-ui/core';
+import { ArticleModel, ID } from 'model';
 import { format } from 'date-fns';
 import { de } from 'date-fns/locale';
 import { AuthorAvatarsList } from './AuthorAvatarsList';
 import { Header } from 'component/general/Header';
+import { User } from 'util/model';
+import { useCurrentUser } from 'util/user/useCurrentUser';
+import { useMutation } from '@apollo/client';
+import { ToggleArticlePinMutation } from 'api/mutation/ToggleArticlePin';
+import { Edit, Place } from '@material-ui/icons';
+import { CollisionLink } from 'component/general/CollisionLink';
+import clsx from 'clsx';
+import { Article as ArticleUtil } from 'util/model/Article';
 
 export interface ArticleTitleProps {
     article: ArticleModel;
 }
 
 const useStyles = makeStyles<Theme>(theme => ({
+    container: {
+        borderBottomStyle: 'solid',
+        borderBottomWidth: 1,
+        borderBottomColor: theme.palette.grey[300],
+        paddingLeft: 0,
+        paddingRight: 0,
+    },
     outerGrid: {
-        height: '100%'
+        height: '100%',
     },
     previewImage: {
-        height: '100%',
-        width: 'auto',
-        maxWidth: '25%',
-        maxHeight: 150,
-        objectFit: 'contain',
-        objectPosition: theme.spacing(0, 1)
+        width: '100%',
+        maxHeight: 200,
+        objectFit: 'cover',
     },
     mainSection: {
-        paddingLeft: theme.spacing(2)
-    },
-    title: {
-        fontSize: '1.6rem',
+        paddingLeft: theme.spacing(1.5),
+        paddingRight: theme.spacing(1),
+        borderRight: '1px solid',
+        borderRightColor: theme.palette.grey[300],
         [theme.breakpoints.down('sm')]: {
-            wordWrap: 'break-word',
-            hyphens: 'auto'
+            border: 0,
+            padding: theme.spacing(0.5),
         }
     },
+    title: {
+        fontSize: '1.3rem',
+        textTransform: 'uppercase',
+        letterSpacing: 2,
+        [theme.breakpoints.down('sm')]: {
+            wordWrap: 'break-word',
+            hyphens: 'auto',
+            fontSize: '1.2rem'
+        }
+    },
+    previewSection: {
+        padding: theme.spacing(1),
+        color: theme.palette.grey[600],
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(0.5),
+        }
+    },
+    buttonSection: {
+        textAlign: 'right',
+        paddingTop: theme.spacing(1),
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(0.5),
+        }
+    },
+    editButton: {
+        color: theme.palette.grey[400],
+    },
+    pinButton: {
+        color: theme.palette.grey[400],
+        '&.active': {
+            color: theme.palette.text.primary
+        }
+    },
+    date: {
+        paddingTop: theme.spacing(1),
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(0.5),
+        }
+    },
+    topic: {
+        border: '1px solid',
+        borderColor: theme.palette.primary.main,
+        color: theme.palette.primary.main,
+        fontSize: '0.7rem',
+        padding: '2px 4px',
+        borderRadius: 4,
+        maxWidth: 'max-content',
+        fontFamily: theme.typography.fontFamily
+    }
 }));
 
 export const ArticleTitle = memo<ArticleTitleProps>(({ article }) => {
     const styles = useStyles();
+    const [currentUser] = useCurrentUser();
+
+    const [toggleArticlePin] = useMutation<{ article: ArticleModel }, { id: ID }>(ToggleArticlePinMutation, {
+        variables: { id: article.id }
+    });
+    const showEditSection = (User.canEditArticle(currentUser, article) || User.isAdmin(currentUser));
     return (
+        <Container className={styles.container}>
         <Header>
-            <Grid container className={styles.outerGrid}>
-                {article.previewImageFile && (
-                    <img
-                        className={styles.previewImage}
-                        src={`https://afdptjdxen.cloudimg.io/cover/320x240/foil1/${article.previewImageFile.remoteLocation}`}
-                        alt={`Vorschaubild zu ${article.title}`}
-                    />
-                )}
-                <Grid item xs className={styles.mainSection}>
-                    <Typography component={'h2'} variant={'h2'} gutterBottom className={styles.title}>
+            <Grid container>
+                <Grid item xs={12} sm={3} container className={styles.outerGrid}>
+                    {article.previewImageFile && (
+                        <img
+                            className={styles.previewImage}
+                            src={`https://afdptjdxen.cloudimg.io/cover/320x240/foil1/${article.previewImageFile.remoteLocation}`}
+                            alt={`Vorschaubild zu ${article.title}`}
+                        />
+                    )}
+                </Grid>
+                <Grid item xs={12} sm={9} className={styles.mainSection}>
+                    <Typography gutterBottom className={styles.title}>
                         {article.title}
                     </Typography>
-                    <Typography component={'div'} variant={'subtitle1'}>
-                        <>&nbsp;<AuthorAvatarsList users={article.users} />&nbsp;</>
-                        {format(new Date(article.updatedAt), 'P', { locale: de }) + ' '}
-                        {article.topic && <>| {article.topic}&nbsp;</>}
-                    </Typography>
+                    <Grid container>
+                        <Grid item xs={12} sm={3}>
+                            <Typography className={styles.date} component={'div'} variant={'subtitle1'}>
+                                <>&nbsp;<AuthorAvatarsList users={article.users} />&nbsp;</>
+                                {format(new Date(article.updatedAt), 'P', { locale: de }) + ' '}
+                            </Typography>
+                            <div className={styles.topic}>
+                            {article.topic && <> {article.topic}</>}
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <Grid container>
+                                <Grid item sm={10}>
+                                    <Typography className={styles.previewSection} variant={'subtitle2'}>
+                                        {article.preview}
+                                    </Typography>
+                                </Grid>
+                                <Grid item sm={2}>
+                                {(article.preview || showEditSection) && (
+                                    <section>
+                                            {showEditSection && (
+                                                <div className={styles.buttonSection}>
+                                                    {User.canEditArticle(currentUser, article) && (
+                                                        <IconButton
+                                                            aria-label="Edit"
+                                                            size="small"
+                                                            className={clsx(styles.editButton, 'edit-button')}
+                                                            component={CollisionLink}
+                                                            to={ArticleUtil.getPath(article, { edit: true })}
+                                                        >
+                                                            <Edit />
+                                                        </IconButton>
+                                                    )}
+                                                    {User.isAdmin(currentUser) && (
+                                                        <IconButton
+                                                            aria-label="Pin"
+                                                            size="small"
+                                                            className={clsx(styles.pinButton, { active: article.isPinnedToTop })}
+                                                            onClick={() => toggleArticlePin()}
+                                                        >
+                                                            <Place />
+                                                        </IconButton>
+                                                    )}
+                                                </div>
+                                            )}
+                                    </section>
+                                )}
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Header>
+        </Container>
     )
 });
