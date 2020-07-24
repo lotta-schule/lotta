c84dbc54740574a2e6fad3f478ff977bcf4c98b1
Christopher Bill <cb.billy.94@gmail.com>
2020-07-24 21:09:01 +0200

    enlarged avatar images

--- src/component/article/AuthorAvatarsList.tsx
+++ src/component/article/AuthorAvatarsList.tsx
@@ -4,6 +4,7 @@ import { makeStyles, Tooltip, Avatar } from '@material-ui/core';
 import { AvatarGroup } from '@material-ui/lab';
 import { User } from 'util/model';
 import clsx from 'clsx';
+import { theme } from 'theme';
 
 export interface AuthorAvatarsListProps {
     users: UserModel[];
@@ -16,12 +17,12 @@ const useStyles = makeStyles(() => ({
         display: 'inline-flex',
     },
     authorAvatar: {
-        width: '1em',
-        height: '1em',
-        borderWidth: 1,
-        '@media screen and (-webkit-min-device-pixel-ratio: 2), screen and (min-resolution: 2dppx)': {
-            borderWidth: .5,
-        }
+        width: '2em',
+        height: '2em',
+        fontSize: '1rem',
+        boxShadow: '1px 1px 7px #0000002e',
+        borderWidth: 0,
+        backgroundColor: theme.palette.grey[400] ,
     },
 }));
 
@@ -31,7 +32,7 @@ export const AuthorAvatarsList = memo<AuthorAvatarsListProps>(({ users, classNam
         return null;
     }
     return (
-        <AvatarGroup max={max ?? 3} classes={{ root: clsx(styles.root, className), avatar: styles.authorAvatar }}>
+        <AvatarGroup max={max ?? 2} classes={{ root: clsx(styles.root, className), avatar: styles.authorAvatar }}>
             {users.map(user => (
                 <Tooltip title={User.getNickname(user)} key={user.id}>
                     <Avatar src={User.getAvatarUrl(user, 40)} />
