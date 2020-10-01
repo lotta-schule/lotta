df42cf785582cd28f7c53fb61e6b1e618c984278
Alexis Rinaldoni <arinaldoni@me.com>
2020-10-01 21:01:28 +0200

    fix reset password

--- src/api/mutation/ResetPasswordMutation.ts
+++ src/api/mutation/ResetPasswordMutation.ts
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 export const ResetPasswordMutation = gql`
     mutation ResetPassword($email: String!, $password: String!, $token: String!) {
         resetPassword(email: $email, password: $password, token: $token) {
-            token
+            accessToken
         }
     }
 `;
