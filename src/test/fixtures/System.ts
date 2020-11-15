7b8b9227a43e76243a6acd169325905ccdde1dc8
Alexis Rinaldoni <arinaldoni@me.com>
2020-11-15 12:58:49 +0100

    Update error / day handling of Schedule Widget, and add tests

--- src/test/fixtures/System.ts
+++ src/test/fixtures/System.ts
@@ -1,5 +1,5 @@
 import { adminGroup, lehrerGroup, elternGroup, schuelerGroup } from './Accounts';
-import { ClientModel } from 'model';
+import { CalendarWidgetConfig, ClientModel, ScheduleWidgetConfig, WidgetModel, WidgetModelType } from 'model';
 
 /**
  *
@@ -271,3 +271,120 @@ export const allCategories = [
     HausaufgabenCategory,
     ArbeitsblätterCategory
 ];
+
+export const VPSchuelerWidget: WidgetModel<ScheduleWidgetConfig> = {
+    id: '91800',
+    title: 'VP Schüler',
+    type: WidgetModelType.VPlan,
+    groups: [],
+    configuration: {
+        type: 'IndiwareStudent',
+        schoolId: '1010',
+        username: 'l3hr3r',
+        password: 'p@ssw0rd'
+    }
+};
+
+export const VPLehrerWidget: WidgetModel<ScheduleWidgetConfig> = {
+    id: '91801',
+    title: 'VP Lehrer',
+    type: WidgetModelType.VPlan,
+    groups: [],
+    configuration: {
+        type: 'IndiwareTeacher',
+        schoolId: '1010',
+        username: 'l3hr3r',
+        password: 'p@ssw0rd'
+    }
+};
+
+export const CalendarKlassenarbeiten: WidgetModel<CalendarWidgetConfig> = {
+    id: '91801',
+    title: 'VP Lehrer',
+    type: WidgetModelType.VPlan,
+    groups: [],
+    configuration: {
+        calendars: [
+            {
+                url: 'http://calendar.com/cal.ical',
+                color: 'green',
+                name: 'Klassenarbeiten 5-6'
+            },
+            {
+                url: 'http://calendar.com/cal2.ical',
+                color: 'green',
+                name: 'Klassenarbeiten 7-10'
+            }
+        ]
+    }
+};
+
+export const ScheduleResponse = {
+    "schedule": {
+        "id": "SCHEDULE:111",
+        "head": {
+            "date": "Montag, 16. November 2020",
+            "filename": "PlanKl20201116.xml",
+            "skipDates": [
+                "Tue, 17 Nov 2020 00:00:00 GMT",
+                "Thu, 19 Nov 2020 00:00:00 GMT",
+                "Fri, 20 Nov 2020 00:00:00 GMT",
+                "Wed, 23 Dec 2020 00:00:00 GMT",
+                "Thu, 24 Dec 2020 00:00:00 GMT",
+                "Fri, 25 Dec 2020 00:00:00 GMT",
+                "Mon, 28 Dec 2020 00:00:00 GMT",
+                "Tue, 29 Dec 2020 00:00:00 GMT",
+                "Wed, 30 Dec 2020 00:00:00 GMT",
+                "Thu, 31 Dec 2020 00:00:00 GMT",
+                "Fri, 01 Jan 2021 00:00:00 GMT",
+                "Mon, 08 Feb 2021 00:00:00 GMT",
+                "Tue, 09 Feb 2021 00:00:00 GMT",
+                "Wed, 10 Feb 2021 00:00:00 GMT",
+            ],
+            "timestamp": "13.11.2020, 12:40",
+            "type": "K"
+        },
+        "body": {
+            "name": "10/1",
+            "short": "10/1",
+            "schedule": [
+                {
+                    "comment": "",
+                    "id": "446",
+                    "lessonIndex": 1,
+                    "lessonName": "DE",
+                    "lessonNameHasChanged": true,
+                    "room": "E107",
+                    "roomHasChanged": true,
+                    "teacher": "ESn",
+                    "teacherHasChanged": false
+                },
+                {
+                    "comment": "Alles Anders",
+                    "id": "464",
+                    "lessonIndex": 8,
+                    "lessonName": "sm15",
+                    "lessonNameHasChanged": false,
+                    "room": "HDS2",
+                    "roomHasChanged": false,
+                    "teacher": "XMei",
+                    "teacherHasChanged": true
+                },
+                {
+                    "comment": "",
+                    "id": "465",
+                    "lessonIndex": 8,
+                    "lessonName": "sw1",
+                    "lessonNameHasChanged": false,
+                    "room": "BSZ2",
+                    "roomHasChanged": false,
+                    "teacher": "Wal",
+                    "teacherHasChanged": false
+                }
+            ]
+        },
+        "footer": {
+            "supervisions": null
+        },
+    }
+};
