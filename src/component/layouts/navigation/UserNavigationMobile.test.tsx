15fb0183afe5894868691185236d0f1f04d2ec5a
Alexis Rinaldoni <arinaldoni@me.com>
2020-12-13 11:41:37 +0100

    fix UserNavigationMobile test (messaging-button adds up)

--- src/component/layouts/navigation/UserNavigationMobile.test.tsx
+++ src/component/layouts/navigation/UserNavigationMobile.test.tsx
@@ -1,59 +1,53 @@
 import React from 'react';
-import {
-    render,
-    cleanup,
-
-} from 'test/util';
+import { render } from 'test/util';
 import { UserNavigationMobile } from './UserNavigationMobile';
 import { GetUnpublishedArticlesQuery } from 'api/query/GetUnpublishedArticles';
 import { SomeUser, adminGroup } from 'test/fixtures';
-
-afterEach(cleanup);
 
 describe('component/layouts/UserNavigationMobile', () => {
 
     describe('logged out user', () => {
         it('should render a login and logout button', () => {
-            const { container, queryByTestId } = render(
+            const screen = render(
                 <UserNavigationMobile />
             );
-            const buttons = container.querySelectorAll('button');
-            expect(buttons.length).toEqual(3);
-            expect(queryByTestId('LoginButton')).not.toBeNull();
-            expect(queryByTestId('RegisterButton')).not.toBeNull();
-            expect(queryByTestId('SearchButton')).not.toBeNull();
+            expect(screen.queryAllByRole('button')).toHaveLength(3);
+            expect(screen.queryByTestId('LoginButton')).not.toBeNull();
+            expect(screen.queryByTestId('RegisterButton')).not.toBeNull();
+            expect(screen.queryByTestId('SearchButton')).not.toBeNull();
 
-            // admin and profile button should not be visible
-            expect(queryByTestId('ProfileButton')).toBeNull();
-            expect(queryByTestId('AdminButton')).toBeNull();
+            // admin and profile and messages button should not be visible
+            expect(screen.queryByTestId('ProfileButton')).toBeNull();
+            expect(screen.queryByTestId('AdminButton')).toBeNull();
+            expect(screen.queryByTestId('MessagingButton')).toBeNull();
         });
     });
 
     describe('non-admin user', () => {
         it('should render profile and createArticle buttons, but not admin buttons', () => {
-            const { container, queryByTestId } = render(
+            const screen = render(
                 <UserNavigationMobile />,
                 {}, { currentUser: SomeUser, useCache: true }
             );
-            const buttons = container.querySelectorAll('button');
-            expect(buttons.length).toEqual(6);
-            expect(queryByTestId('LoginButton')).toBeNull();
-            expect(queryByTestId('RegisterButton')).toBeNull();
+            expect(screen.queryAllByRole('button')).toHaveLength(7);
+            expect(screen.queryByTestId('LoginButton')).toBeNull();
+            expect(screen.queryByTestId('RegisterButton')).toBeNull();
 
-            expect(queryByTestId('SearchButton')).toBeVisible();
-            expect(queryByTestId('CreateArticleButton')).toBeVisible();
-            expect(queryByTestId('OwnArticlesButton')).toBeVisible();
-            expect(queryByTestId('ProfileButton')).toBeVisible();
-            expect(queryByTestId('ProfileFilesButton')).toBeVisible();
+            expect(screen.queryByTestId('SearchButton')).toBeVisible();
+            expect(screen.queryByTestId('CreateArticleButton')).toBeVisible();
+            expect(screen.queryByTestId('OwnArticlesButton')).toBeVisible();
+            expect(screen.queryByTestId('ProfileButton')).toBeVisible();
+            expect(screen.queryByTestId('ProfileFilesButton')).toBeVisible();
+            expect(screen.queryByTestId('MessagingButton')).toBeVisible();
 
             // admin and profile button should not be visible
-            expect(queryByTestId('AdminButton')).toBeNull();
+            expect(screen.queryByTestId('AdminButton')).toBeNull();
         });
     });
 
     describe('admin user', () => {
         it('should render profile and createArticle buttons, and also admin buttons', () => {
-            const { container, queryByTestId } = render(
+            const screen = render(
                 <UserNavigationMobile />,
                 {},
                 {
@@ -62,19 +56,19 @@ describe('component/layouts/UserNavigationMobile', () => {
                     useCache: true
                 }
             );
-            const buttons = container.querySelectorAll('button');
-            expect(buttons.length).toEqual(8);
-            expect(queryByTestId('LoginButton')).toBeNull();
-            expect(queryByTestId('RegisterButton')).toBeNull();
+            expect(screen.queryAllByRole('button')).toHaveLength(9);
+            expect(screen.queryByTestId('LoginButton')).toBeNull();
+            expect(screen.queryByTestId('RegisterButton')).toBeNull();
 
-            expect(queryByTestId('SearchButton')).toBeVisible();
-            expect(queryByTestId('CreateArticleButton')).toBeVisible();
-            expect(queryByTestId('OwnArticlesButton')).toBeVisible();
-            expect(queryByTestId('ProfileButton')).toBeVisible();
-            expect(queryByTestId('ProfileFilesButton')).toBeVisible();
+            expect(screen.queryByTestId('SearchButton')).toBeVisible();
+            expect(screen.queryByTestId('CreateArticleButton')).toBeVisible();
+            expect(screen.queryByTestId('OwnArticlesButton')).toBeVisible();
+            expect(screen.queryByTestId('ProfileButton')).toBeVisible();
+            expect(screen.queryByTestId('ProfileFilesButton')).toBeVisible();
+            expect(screen.queryByTestId('MessagingButton')).toBeVisible();
 
-            expect(queryByTestId('ProfileButton')).toBeVisible();
-            expect(queryByTestId('AdminButton')).toBeVisible();
+            expect(screen.queryByTestId('ProfileButton')).toBeVisible();
+            expect(screen.queryByTestId('AdminButton')).toBeVisible();
         });
     });
 });
