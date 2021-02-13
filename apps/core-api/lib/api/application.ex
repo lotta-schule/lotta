549319490ff324b8952910fde900a40c6b45ecf1
Alexis Rinaldoni <arinaldoni@me.com>
2021-02-13 01:02:23 +0100

    application.ex code format

--- apps/core-api/lib/api/application.ex
+++ apps/core-api/lib/api/application.ex
@@ -10,32 +10,32 @@ defmodule Api.Application do
 
     # List all child processes to be supervised
     children =
-      ([
-         Api.Repo,
-         ApiWeb.Telemetry,
-         {Phoenix.PubSub, name: Api.PubSub, adapter: Phoenix.PubSub.PG2},
-         ApiWeb.Endpoint,
-         {Absinthe.Subscription, ApiWeb.Endpoint},
-         {Redix, Application.fetch_env!(:api, :redis_connection)},
-         Api.Elasticsearch.Cluster,
-         Api.Queue.MediaConversionRequestPublisher,
-         Api.Queue.MediaConversionConsumer,
-         {ConCache,
-          name: :http_cache, ttl_check_interval: :timer.hours(1), global_ttl: :timer.hours(4)}
-       ] ++
-         cond do
-           environment == :development ->
-             [Api.System.DefaultContent]
+      [
+        Api.Repo,
+        ApiWeb.Telemetry,
+        {Phoenix.PubSub, name: Api.PubSub, adapter: Phoenix.PubSub.PG2},
+        ApiWeb.Endpoint,
+        {Absinthe.Subscription, ApiWeb.Endpoint},
+        {Redix, Application.fetch_env!(:api, :redis_connection)},
+        Api.Elasticsearch.Cluster,
+        Api.Queue.MediaConversionRequestPublisher,
+        Api.Queue.MediaConversionConsumer,
+        {ConCache,
+         name: :http_cache, ttl_check_interval: :timer.hours(1), global_ttl: :timer.hours(4)}
+      ] ++
+        cond do
+          environment == :development ->
+            [Api.System.DefaultContent]
 
-           environment == :production ->
-             [Api.System.DefaultContent]
+          environment == :production ->
+            [Api.System.DefaultContent]
 
-           environment == :test ->
-             []
+          environment == :test ->
+            []
 
-           true ->
-             []
-         end)
+          true ->
+            []
+        end
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
