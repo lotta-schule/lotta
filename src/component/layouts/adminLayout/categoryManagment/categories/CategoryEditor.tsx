ffff5fa82ae2b5ed3eb9080c5f2cd15161758ad4
Alexis Rinaldoni <arinaldoni@me.com>
2021-05-10 22:23:58 +0200

    Admin: CategoryManagment: add possibility to set externel redirect of categories

--- src/component/layouts/adminLayout/categoryManagment/categories/CategoryEditor.tsx
+++ src/component/layouts/adminLayout/categoryManagment/categories/CategoryEditor.tsx
@@ -11,6 +11,8 @@ import {
     MenuItem,
     Checkbox,
     FormControlLabel,
+    RadioGroup,
+    Radio,
 } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
 import { CategoryModel, WidgetModel, ID } from 'model';
@@ -21,20 +23,23 @@ import { SelectFileOverlay } from 'component/edit/SelectFileOverlay';
 import { PlaceholderImage } from 'component/placeholder/PlaceholderImage';
 import { ErrorMessage } from 'component/general/ErrorMessage';
 import { useCategories } from 'util/categories/useCategories';
-import { Category } from 'util/model';
+import { Category, RedirectType } from 'util/model';
 import { CategoryWidgetSelector } from './CategoryWidgetSelector';
 import { DeleteCategoryDialog } from './DeleteCategoryDialog';
 import { GetCategoryWidgetsQuery } from 'api/query/GetCategoryWidgetsQuery';
+import { Button } from 'component/general/button/Button';
+import { animated, useSpring } from 'react-spring';
 import clsx from 'clsx';
 import Img from 'react-cloudimage-responsive';
-import { Button } from 'component/general/button/Button';
 
 const useStyles = makeStyles((theme: Theme) => ({
     input: {
-        marginTop: theme.spacing(3),
+        marginTop: theme.spacing(1.5),
         width: '100%',
     },
     title: {
+        marginTop: theme.spacing(5),
+        paddingTop: theme.spacing(3),
         marginBottom: theme.spacing(1),
     },
     switchBase: {
@@ -97,11 +102,11 @@ export const CategoryEditor = React.memo<CategoryEditorProps>(
             }
         }, [currentWidgetsData]);
 
-        const updateCategory = React.useCallback(async () => {
+        const updateCategory = React.useCallback(() => {
             if (!selectedCategory || !category) {
                 return null;
             }
-            mutateCategory({
+            return mutateCategory({
                 variables: {
                     id: selectedCategory.id,
                     category: {
@@ -110,10 +115,7 @@ export const CategoryEditor = React.memo<CategoryEditorProps>(
                             id: category.bannerImageFile.id,
                         },
                         groups: category.groups?.map(({ id }) => ({ id })),
-                        redirect:
-                            category.redirect === 'null'
-                                ? null
-                                : category.redirect,
+                        redirect: category.redirect,
                         layoutName: category.layoutName,
                         hideArticlesFromHomepage:
                             category.hideArticlesFromHomepage || false,
@@ -123,6 +125,21 @@ export const CategoryEditor = React.memo<CategoryEditorProps>(
                 },
             });
         }, [category, mutateCategory, selectedCategory, selectedWidgets]);
+
+        const redirectInternallySpringProps = useSpring({
+            overflow: 'hidden',
+            height:
+                Category.getRedirectType(category) === RedirectType.Intern
+                    ? 70
+                    : 0,
+        });
+        const redirectExternallySpringProps = useSpring({
+            overflow: 'hidden',
+            height:
+                Category.getRedirectType(category) === RedirectType.Extern
+                    ? 70
+                    : 0,
+        });
 
         React.useEffect(() => {
             if (selectedCategory === null && category !== null) {
@@ -193,25 +210,29 @@ export const CategoryEditor = React.memo<CategoryEditorProps>(
                 </SelectFileOverlay>
 
                 {!category.isHomepage && (
-                    <FormControl className={styles.input}>
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    checked={category.hideArticlesFromHomepage}
-                                    onChange={(_, checked) =>
-                                        setCategory({
-                                            ...category,
-                                            hideArticlesFromHomepage: checked,
-                                        })
-                                    }
-                                    value={'hideArticlesFromHomepage'}
-                                />
-                            }
-                            label={
-                                'Beiträge dieser Kategorie auf der Startseite verstecken'
-                            }
-                        />
-                    </FormControl>
+                    <>
+                        <FormControl className={styles.input}>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={
+                                            category.hideArticlesFromHomepage
+                                        }
+                                        onChange={(_, checked) =>
+                                            setCategory({
+                                                ...category,
+                                                hideArticlesFromHomepage: checked,
+                                            })
+                                        }
+                                        value={'hideArticlesFromHomepage'}
+                                    />
+                                }
+                                label={
+                                    'Beiträge dieser Kategorie auf der Startseite verstecken'
+                                }
+                            />
+                        </FormControl>
+                    </>
                 )}
 
                 <FormControl className={styles.input}>
@@ -242,37 +263,120 @@ export const CategoryEditor = React.memo<CategoryEditorProps>(
 
                 {!category.isHomepage && (
                     <>
-                        <FormControl className={styles.input}>
-                            <InputLabel htmlFor={'category-redirect'}>
-                                Zu einer anderen Kategorie weiterleiten ...
-                            </InputLabel>
-                            <Select
-                                value={category.redirect || 'null'}
-                                onChange={({ target }) =>
+                        <Typography
+                            className={clsx(styles.input, styles.title)}
+                            variant={'h6'}
+                        >
+                            Die Kategorie als Weiterleitung
+                        </Typography>
+
+                        <RadioGroup
+                            aria-label={'Die Kategorie als Weiterleitung'}
+                            value={Category.getRedirectType(category)}
+                            onChange={(_e, value) => {
+                                if (value === RedirectType.None) {
                                     setCategory({
                                         ...category,
-                                        redirect: target.value as string,
-                                    })
+                                        redirect: undefined,
+                                    });
                                 }
-                                inputProps={{
-                                    id: 'category-redirect',
-                                }}
-                                displayEmpty
-                                fullWidth
+                                if (value === RedirectType.Intern) {
+                                    setCategory({
+                                        ...category,
+                                        redirect: Category.getPath(
+                                            categories[0]
+                                        ),
+                                    });
+                                }
+                                if (value === RedirectType.Extern) {
+                                    setCategory({
+                                        ...category,
+                                        redirect: 'https://lotta.schule',
+                                    });
+                                }
+                            }}
+                        >
+                            <FormControlLabel
+                                value={RedirectType.None}
+                                control={<Radio />}
+                                label={
+                                    'Kategorie wird nicht weitergeleitet und zeigt eigene Beiträge an.'
+                                }
+                            />
+                            <FormControlLabel
+                                value={RedirectType.Intern}
+                                control={<Radio />}
+                                label={
+                                    'Kategorie zu einer anderen Kategorie weiterleiten:'
+                                }
+                            />
+
+                            <animated.div
+                                data-testid={'InternalRedirectWrapper'}
+                                style={redirectInternallySpringProps}
                             >
-                                <MenuItem value={'null'}>
-                                    <em>Nicht weiterleiten</em>
-                                </MenuItem>
-                                {categories.map((category) => (
-                                    <MenuItem
-                                        key={category.id}
-                                        value={Category.getPath(category)}
+                                <FormControl className={styles.input}>
+                                    <InputLabel htmlFor={'category-redirect'}>
+                                        Zu einer anderen Kategorie weiterleiten
+                                        ...
+                                    </InputLabel>
+                                    <Select
+                                        value={category.redirect || 'null'}
+                                        onChange={({ target }) =>
+                                            setCategory({
+                                                ...category,
+                                                redirect: target.value as string,
+                                            })
+                                        }
+                                        inputProps={{
+                                            id: 'category-redirect',
+                                        }}
+                                        displayEmpty
+                                        fullWidth
                                     >
-                                        {category.title}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
+                                        {categories.map((category) => (
+                                            <MenuItem
+                                                key={category.id}
+                                                value={Category.getPath(
+                                                    category
+                                                )}
+                                            >
+                                                {category.title}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </animated.div>
+
+                            <FormControlLabel
+                                value={RedirectType.Extern}
+                                control={<Radio />}
+                                label={
+                                    'Kategorie zu einer Seite im Internet weiterleiten'
+                                }
+                            />
+
+                            <animated.div
+                                data-testid={'ExternalRedirectWrapper'}
+                                style={redirectExternallySpringProps}
+                            >
+                                <TextField
+                                    fullWidth
+                                    className={styles.input}
+                                    label={'Ziel der Weiterleitung:'}
+                                    inputProps={{
+                                        'aria-label': 'Ziel der Weiterleitung',
+                                    }}
+                                    value={category.redirect}
+                                    onChange={(e) =>
+                                        setCategory({
+                                            ...category,
+                                            redirect: e.target.value,
+                                        })
+                                    }
+                                />
+                            </animated.div>
+                        </RadioGroup>
 
                         <p>&nbsp;</p>
                     </>
