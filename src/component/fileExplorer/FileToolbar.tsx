839a85fe4a06b6b1005676f8cea28127f280600d
Alexis <arinaldoni@me.com>
2021-05-29 15:12:14 +0200

    FileToolbar: Fix upload button positioning

--- src/component/fileExplorer/FileToolbar.tsx
+++ src/component/fileExplorer/FileToolbar.tsx
@@ -58,6 +58,8 @@ const useStyles = makeStyles<Theme>((theme: Theme) =>
         },
         uploadButton: {
             position: 'absolute',
+            left: 0,
+            top: 0,
             width: '100%',
             height: '100%',
             opacity: 0,
