fd15c9b5c35475ba5e5fa4ae25218a287a4a8fea
Alexis <arinaldoni@me.com>
2021-12-27 10:48:20 +0100

    add a Toolbar component

---
+++ src/shared/general/layout/Toolbar.test.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from 'test/util';
+import { Toolbar } from './Toolbar';
+
+describe('Toolbar', () => {
+    it('should render', () => {
+        render(<Toolbar />);
+    });
+});
