f825d1b75e356f153d4f11dc85f4ab785e7068a0
Christopher Bill <cb.billy.94@gmail.com>
2023-02-08 19:25:26 +0100

    added description text for image edit (preview image)

--- src/article/preview/ArticlePreviewStandardLayout.tsx
+++ src/article/preview/ArticlePreviewStandardLayout.tsx
@@ -108,6 +108,7 @@ export const ArticlePreviewStandardLayout = React.memo<ArticlePreviewProps>(
                                 allowDeletion
                                 style={{ width: '100%' }}
                                 label={'Vorschaubild ändern'}
+                                description={'opt. Größe 1200px x 800px'}
                                 onSelectFile={(previewImageFile) =>
                                     onUpdateArticle({
                                         ...article,
