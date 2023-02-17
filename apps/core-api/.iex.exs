42da467e4526827a9df7e9e9aa2a9be0a2965fe0
Alexis Rinaldoni <arinaldoni@me.com>
2023-02-17 21:38:55 +0100

    Add github actions

--- apps/core-api/.iex.exs
+++ apps/core-api/.iex.exs
@@ -2,7 +2,7 @@ alias Lotta.{Accounts, Content, Messages, Storage, Slugifier, Tenants, Repo}
 alias Lotta.Accounts.{Perrmissions, UserGroup, User}
 alias Lotta.Content.{Article, ContentModule, ContentModuleResult}
 alias Lotta.Messages.{Conversation, Message}
-alias Lotta.Storage.{Directory, File, FileConversion, RemoteStorage, RemoteStorageEntity, Storage}
+alias Lotta.Storage.{Directory, FileConversion, RemoteStorage, RemoteStorageEntity, Storage}
 alias Lotta.Tenants.{Category, Configuration, CustomDomain, Tenant, TenantSelector, Usage, Widget}
 
 alias Ecto.Changeset
