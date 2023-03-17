a0c55cdcc9ed1743d5c96a6eb68228ed03236c1a
Alexis Rinaldoni <arinaldoni@me.com>
2023-03-18 00:27:53 +0100

    add OpenTelemetry

---
+++ server.js
@@ -0,0 +1,23 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
+    await app.prepare();
+
+    const server = createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        handle(req, res, parsedUrl);
+    });
+
+    server.listen(port, () => {
+        console.log(`> Lotta listening on http://localhost:${port}`);
+    });
+};
+
+startServer();
