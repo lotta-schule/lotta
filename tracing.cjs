e37cedb4568b4f9be07fa87abbef991d2b766b7a
Alexis Rinaldoni <arinaldoni@me.com>
2023-07-29 02:05:45 +0200

    Make tracing a normal boring old commonjs module file

--- tracing.cjs
+++ tracing.cjs
@@ -1,10 +1,10 @@
-import OpenTelemetryNode from '@opentelemetry/sdk-node';
-import OpentelemetryExporterTraceHttp from '@opentelemetry/exporter-trace-otlp-http';
-import OpenTelemetryResources from '@opentelemetry/resources';
-import OpenTelemetrySemanticConventions from '@opentelemetry/semantic-conventions';
-import OpenTelemetryInstrumentationHttp from '@opentelemetry/instrumentation-http';
-import OpenTelemetryInstrumentationFetch from '@opentelemetry/instrumentation-fetch';
-import OpenTelemetryInstrumentationConnect from '@opentelemetry/instrumentation-connect';
+const OpenTelemetryNode = require('@opentelemetry/sdk-node');
+const OpentelemetryExporterTraceHttp = require('@opentelemetry/exporter-trace-otlp-http');
+const OpenTelemetryResources = require('@opentelemetry/resources');
+const OpenTelemetrySemanticConventions = require('@opentelemetry/semantic-conventions');
+const OpenTelemetryInstrumentationHttp = require('@opentelemetry/instrumentation-http');
+const OpenTelemetryInstrumentationFetch = require('@opentelemetry/instrumentation-fetch');
+const OpenTelemetryInstrumentationConnect = require('@opentelemetry/instrumentation-connect');
 
 const { SemanticResourceAttributes } = OpenTelemetrySemanticConventions;
 const { Resource } = OpenTelemetryResources;
@@ -16,7 +16,7 @@ const { ConnectInstrumentation } = OpenTelemetryInstrumentationConnect;
 
 // configure the SDK to export telemetry data to the console
 // enable all auto-instrumentations from the meta package
-const exporterOptions = {
+const exporterOptions  = {
     url: 'http://tempo.monitoring:4318/v1/traces',
 };
 const traceExporter = new OTLPTraceExporter(exporterOptions);
@@ -43,4 +43,3 @@ process.on('SIGTERM', () => {
         .finally(() => process.exit(0));
 });
 
-export const tracing = sdk;
