475a8edf194c348f8be5b09bad6baded2ee37cf4
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2023-09-01 22:23:10 +0200

    Fix storybook-addon-theme

--- jest.base-config.cjs
+++ jest.base-config.cjs
@@ -3,67 +3,69 @@
  * https://jestjs.io/docs/configuration
  */
 
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ * @see https://jestjs.io/docs/configuration
+ **/
 const jestConfig = {
-    clearMocks: true,
+  clearMocks: true,
 
-    collectCoverage: false,
-    collectCoverageFrom: [
-        'src/**/*.{js,jsx,ts,tsx}',
-        '/index.ts$',
-        'node_modules/**',
-        '!**/*.d.ts',
-    ],
-    coverageDirectory: './coverage/jest/',
-    coveragePathIgnorePatterns: ['/node_modules/'],
-    coverageProvider: 'babel',
+  reporters: ['default', 'jest-junit'],
+  testResultsProcessor: 'jest-junit',
 
-    coverageReporters: ['text', 'cobertura'],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/**',
+    '!**/*.d.ts',
+  ],
+  coverageDirectory: './coverage/jest/',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageProvider: 'babel',
+  coverageReporters: ['text', 'cobertura'],
 
-    moduleFileExtensions: [
-        'js',
-        'mjs',
-        'cjs',
-        'jsx',
-        'ts',
-        'tsx',
-        'json',
-        'node',
-    ],
+  moduleFileExtensions: [
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+  ],
 
-    moduleNameMapper: {
-        '\\.(scss|sass|css)$': 'identity-obj-proxy',
-        '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/fileMock.js',
-    },
-    roots: ['<rootDir>/src'],
-    moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/fileMock.js',
+  },
+  roots: ['<rootDir>/src'],
+  moduleDirectories: ['node_modules', 'src'],
 
-    setupFilesAfterEnv: ['./jest.setup.ts'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 
-    testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom',
 
-    testMatch: [
-        '**/__tests__/**/*.[jt]s?(x)',
-        '**/?(*.)+(spec|test).[tj]s?(x)',
-    ],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': [
-            'babel-jest',
-            {
-                presets: [
-                    ['@babel/preset-react', { runtime: 'automatic' }],
-                    '@babel/preset-typescript',
-                    ['@babel/preset-env', { targets: { node: 'current' } }],
-                ],
-            },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-react', { runtime: 'automatic' }],
+          '@babel/preset-typescript',
+          ['@babel/preset-env', { targets: { node: 'current' } }],
         ],
-    },
-
-    transformIgnorePatterns: [
-        '/node_modules/',
-        '\\.pnp\\.[^\\/]+$',
-        '^.+\\.module\\.(css|sass|scss)$',
+      },
     ],
+  },
+
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '\\.pnp\\.[^\\/]+$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
 };
 
 module.exports = jestConfig;
--- packages/storybook-addon-theme/jest.config.cjs
+++ packages/storybook-addon-theme/jest.config.cjs
@@ -1,7 +1,11 @@
 const baseConfig = require('../../jest.base-config.cjs');
 
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ * @see https://jestjs.io/docs/configuration
+ **/
 const jestConfig = {
-    ...baseConfig,
+  ...baseConfig,
 };
 
 module.exports = jestConfig;
