68ef8ab90965a3ef9267221fa0f10f7d199dd9fe
Alexis Rinaldoni <arinaldoni@me.com>
2023-09-02 00:52:25 +0200

    v4.0.1

--- apps/core-api/mix.exs
+++ apps/core-api/mix.exs
@@ -4,7 +4,7 @@ defmodule Lotta.MixProject do
   def project do
     [
       app: :lotta,
-      version: "4.0.0",
+      version: "4.0.1",
       name: "Lotta API Server",
       source_url: "https://github.com/lotta-schule/core",
       homepage_url: "https://lotta.schule",
