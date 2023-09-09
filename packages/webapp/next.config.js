1c33594cab45cde667000efc87377ed3b23c66a4
Alexis Rinaldoni <arinaldoni@me.com>
2023-09-09 16:40:50 +0200

    Remove debug code

--- packages/webapp/next.config.js
+++ packages/webapp/next.config.js
@@ -56,13 +56,7 @@ const nextConfig = {
   eslint: {
     dirs: ['src'],
   },
-  experimental: {
-    logging: 'verbose',
-  },
   generateEtags: false,
-  httpAgentOptions: {
-    keepAlive: false,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.test.(t|j)sx?$/,
--- packages/webapp/server.js
+++ packages/webapp/server.js
@@ -1,21 +1,7 @@
 const { createServer } = require('node:http');
 const { parse } = require('node:url');
-const { Console } = require('node:console');
 
-const console = new Console({ stdout: process.stdout, stderr: process.stderr });
 const port = process.env.PORT || 3000;
-
-const originalFetch = globalThis.fetch;
-
-globalThis.fetch = async (url, options) => {
-  console.trace('fetch');
-  console.log({ url, ...options });
-  return originalFetch(url, options).then((res) => {
-    console.log('fetch result: ');
-    console.log({ res });
-    return res;
-  });
-};
 
 const startServer = async () => {
   require('./tracing.cjs');
