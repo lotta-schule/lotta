75cc7ba58d12741736c6da7b6d138a2faadb6cf6
Alexis Rinaldoni <arinaldoni@me.com>
2023-09-09 04:18:57 +0200

    override fetch

--- packages/webapp/fetch.cjs
+++ packages/webapp/fetch.cjs
@@ -13,18 +13,16 @@ module.exports = () =>
       Request,
       Response,
     }) => {
-      if (!globalThis.fetch) {
-        globalThis.fetch = fetch;
-        globalThis.Headers = Headers;
-        globalThis.Request = Request;
-        globalThis.Response = Response;
-        globalThis.Blob = Blob;
-        globalThis.blobFrom = blobFrom;
-        globalThis.blobFromSync = blobFromSync;
-        globalThis.File = File;
-        globalThis.fileFrom = fileFrom;
-        globalThis.fileFromSync = fileFromSync;
-        globalThis.FormData = FormData;
-      }
+      globalThis.fetch = fetch;
+      globalThis.Headers = Headers;
+      globalThis.Request = Request;
+      globalThis.Response = Response;
+      globalThis.Blob = Blob;
+      globalThis.blobFrom = blobFrom;
+      globalThis.blobFromSync = blobFromSync;
+      globalThis.File = File;
+      globalThis.fileFrom = fileFrom;
+      globalThis.fileFromSync = fileFromSync;
+      globalThis.FormData = FormData;
     }
   );
