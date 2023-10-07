3dd94229241cfcefa4561f12efb43a4efdce2e1f
Alexis Rinaldoni <arinaldoni@me.com>
2023-10-07 14:26:47 +0200

    APNS set category

--- apps/core-api/lib/lotta/notification/push_notification.ex
+++ apps/core-api/lib/lotta/notification/push_notification.ex
@@ -46,6 +46,7 @@ defmodule Lotta.Notification.PushNotification do
           |> put_alert(conversation, message)
           |> Notification.put_sound("default")
           |> Notification.put_thread_id("#{tenant.slug}/#{conversation.id}")
+          |> Notification.put_category("receive_message")
           |> Notification.put_custom(%{conversation_id: conversation.id, message_id: message.id})
           |> Lotta.Notification.Provider.APNS.push()
 
@@ -62,15 +63,15 @@ defmodule Lotta.Notification.PushNotification do
     |> then(&Notification.put_alert(notification, &1))
   end
 
-  defp put_alert_title(alert, conversation, message),
+  defp put_alert_title(alert, _, message),
     do: Map.put(alert, :title, message.user.name)
 
-  defp put_alert_subtitle(alert, %Conversation{groups: [group]}, message),
+  defp put_alert_subtitle(alert, %Conversation{groups: [group]}, _),
     do: Map.put(alert, :subtitle, "in #{group.name}")
 
   defp put_alert_subtitle(alert, _, _), do: alert
 
-  defp put_alert_body(alert, conversation, %Message{content: content}),
+  defp put_alert_body(alert, _, %Message{content: content}),
     do: Map.put(alert, :body, String.slice(content, 0, 100))
 
   defp put_alert_body(alert, _, _), do: alert
