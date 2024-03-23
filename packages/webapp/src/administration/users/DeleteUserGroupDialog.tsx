e65555166ee9a69a2899ad723518f88a10b7041b
JP <153733311+techotline@users.noreply.github.com>
2024-03-23 11:25:30 +0100

    add some gap

--- packages/webapp/src/administration/users/DeleteUserGroupDialog.tsx
+++ packages/webapp/src/administration/users/DeleteUserGroupDialog.tsx
@@ -95,7 +95,7 @@ export const DeleteUserGroupDialog = React.memo(
                 <p>
                   Beiträge, die <em>ausschließlich</em> für diese
                   Gruppe sichtbar waren, werden wieder in den Status "zur Kontrolle freigegeben"
-                  zurückversetzt und müssen neuen Gruppen zugewiesen werden.
+                  zurückversetzt und müssen neuen Gruppen zugewiesen werden,
                   um sichtbar zu sein.
                 </p>
               </>
