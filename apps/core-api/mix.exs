43702eeedb429a1175e1af3d039c481ce805e21c
Alexis Rinaldoni <arinaldoni@me.com>
2024-06-28 11:14:33 +0200

    Update formatter

--- apps/core-api/mix.exs
+++ apps/core-api/mix.exs
@@ -22,7 +22,7 @@ defmodule Lotta.MixProject do
       ],
       test_coverage: [
         tool: ExCoveralls,
-        output: "coverage",
+        output: "coverage"
       ],
       preferred_cli_env: [
         coveralls: :test,
