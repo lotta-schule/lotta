9501201c46a7192778c8c90a5cead665e6913f45
Alexis Rinaldoni <arinaldoni@me.com>
2024-08-15 23:01:15 +0200

    Add Create event dialog

--- apps/webapp/src/app/(admin)/admin/calendars/_component/CreateEventDialog.tsx
+++ apps/webapp/src/app/(admin)/admin/calendars/_component/CreateEventDialog.tsx
@@ -12,7 +12,18 @@ import {
   Collapse,
 } from '@lotta-schule/hubert';
 import { useTranslation } from 'react-i18next';
-import { useQuery } from '@apollo/client';
+import { useSuspenseQuery } from '@apollo/client';
+import {
+  addDays,
+  addHours,
+  endOfDay,
+  format,
+  isSameDay,
+  startOfHour,
+} from 'date-fns';
+import clsx from 'clsx';
+
+import styles from './CreateEventDialog.module.scss';
 
 import GetCalendarsQuery from 'api/query/GetCalendarsQuery.graphql';
 
@@ -23,60 +34,185 @@ export type CreateEventDialogProps = {
 
 export const CreateEventDialog = React.memo(
   ({ isOpen, onClose }: CreateEventDialogProps) => {
+    const {
+      data: { calendars },
+    } = useSuspenseQuery<{ calendars: { id: string; name: string }[] }>(
+      GetCalendarsQuery,
+      {
+        fetchPolicy: 'cache-first',
+      }
+    );
+
     const { t } = useTranslation();
     const isLoading = false;
-    const [isAllDay, setIsAllDay] = React.useState(false);
+    const [calendarId, setCalendarId] = React.useState(calendars.at(0)?.id);
+    const [date, setDate] = React.useState(() => startOfHour(new Date()));
+    const [endDate, setEndDate] = React.useState<Date>(() => addHours(date, 1));
+    const [isAllDay, setIsAllDay] = React.useState(true);
+    const [repetition, setRepetition] = React.useState('');
 
-    const { data } = useQuery(GetCalendarsQuery, {
-      ssr: false,
-      fetchPolicy: 'cache-first',
-    });
+    const isMultipleDays = React.useMemo(
+      () => !isSameDay(date, endDate),
+      [date, endDate]
+    );
 
     return (
-      <Dialog open={isOpen} onRequestClose={onClose} title={t('Create event')}>
+      <Dialog
+        open={isOpen}
+        onRequestClose={onClose}
+        title={t('Create event')}
+        className={styles.root}
+      >
         <form>
           <DialogContent>
-            <Label label={t('name')}>
-              <Input autoFocus={isOpen} id="name" disabled={isLoading} />
-            </Label>
+            <div className={clsx(styles.row, styles.nameRow)}>
+              <Label label={t('name')} style={{ flex: 1 }}>
+                <Input autoFocus={isOpen} id="name" disabled={isLoading} />
+              </Label>
+            </div>
             <Label label={t('description')}>
               <Input multiline id="description" disabled={isLoading} />
             </Label>
-            <div style={{ display: 'flex' }}>
-              <Select title={t('calendar')} value={''} onChange={() => {}}>
-                {data?.calendars.map((calendar: any) => (
+
+            <div className={styles.configRow}>
+              <Select
+                title={t('calendar')}
+                value={calendarId}
+                onChange={setCalendarId}
+              >
+                {calendars.map((calendar: any) => (
                   <Option key={calendar.id} value={calendar.id}>
                     {calendar.name}
                   </Option>
                 ))}
               </Select>
-              <Checkbox
-                id="allDay"
-                isDisabled={isLoading}
-                isSelected={isAllDay}
-                onChange={setIsAllDay}
-                style={{ paddingTop: '2em' }}
+
+              <Select
+                title={t('repetition')}
+                value={repetition}
+                onChange={setRepetition}
               >
-                {t('all-day')}
-              </Checkbox>
+                <Option key={'none'} value={''}>
+                  {t('none')}
+                </Option>
+                <Option key={'daily'} value={'daily'}>
+                  {t('daily')}
+                </Option>
+                <Option key={'weekly'} value={'weekly'}>
+                  {t('weekly')}
+                </Option>
+                <Option key={'biweekly'} value={'biweekly'}>
+                  {t('biweekly')}
+                </Option>
+                <Option key={'monthly'} value={'monthly'}>
+                  {t('monthly')}
+                </Option>
+                <Option key={'yearly'} value={'yearly'}>
+                  {t('yearly')}
+                </Option>
+              </Select>
+              <div>
+                <Checkbox
+                  id="allDay"
+                  isDisabled={isLoading}
+                  isSelected={isAllDay}
+                  onChange={setIsAllDay}
+                >
+                  {t('all-day')}
+                </Checkbox>
+                <Checkbox
+                  id="multipleDays"
+                  isDisabled={isLoading}
+                  isSelected={isMultipleDays}
+                  onChange={(selected) => {
+                    if (selected) {
+                      setEndDate(endOfDay(addDays(date, 1)));
+                    } else {
+                      setEndDate(addHours(date, 1));
+                    }
+                  }}
+                  className={clsx(styles.mayBeInvisible, {
+                    [styles.isVisible]: isAllDay,
+                  })}
+                >
+                  {t('multiple days')}
+                </Checkbox>
+              </div>
             </div>
 
-            <div style={{ display: 'flex' }}>
-              <Label label={t('start date')} style={{ flexGrow: 1 }}>
-                <Input type="startdate" disabled={isLoading} />
+            <div className={styles.dateRow}>
+              <Label label={t('date')} style={{ flexGrow: 1 }}>
+                <Input
+                  type="date"
+                  id="startdate"
+                  value={date.toISOString().split('T')[0]}
+                  onChange={(e) => {
+                    setDate((d) => {
+                      const newDate = e.currentTarget.valueAsDate;
+                      if (newDate) {
+                        newDate.setHours(d.getHours());
+                        newDate.setMinutes(d.getMinutes());
+                        return newDate;
+                      }
+                      return d;
+                    });
+                  }}
+                  disabled={isLoading}
+                />
               </Label>
-              <Label label={t('end date')} style={{ flexGrow: 1 }}>
-                <Input type="enddate" id="enddate" disabled={isLoading} />
+              <Label
+                label={t('start time')}
+                style={{ flexGrow: 1 }}
+                className={clsx(styles.mayBeInvisible, {
+                  [styles.isVisible]: !isAllDay,
+                })}
+              >
+                <Input
+                  type="time"
+                  id="starttime"
+                  disabled={isLoading}
+                  value={format(date, 'HH:mm')}
+                  onChange={(e) => {
+                    const newTime = e.currentTarget.valueAsDate;
+                    console.log({ newTime });
+                  }}
+                />
+              </Label>
+              <Label
+                label={t('end time')}
+                style={{ flexGrow: 1 }}
+                className={clsx(styles.mayBeInvisible, {
+                  [styles.isVisible]: !isAllDay,
+                })}
+              >
+                <Input
+                  type="time"
+                  id="endtime"
+                  disabled={isLoading}
+                  value={endDate ? format(endDate, 'HH:mm') : ''}
+                  onChange={(e) => {
+                    const newTime = e.currentTarget.valueAsDate;
+                    console.log({ newTime });
+                  }}
+                />
               </Label>
             </div>
 
-            <Collapse isOpen={isAllDay}>
-              <div style={{ display: 'flex' }}>
-                <Label label={t('start time')} style={{ flexGrow: 1 }}>
-                  <Input type="starttime" disabled={isLoading} />
-                </Label>
-                <Label label={t('end time')} style={{ flexGrow: 1 }}>
-                  <Input type="endtime" id="endtime" disabled={isLoading} />
+            <Collapse isOpen={isMultipleDays}>
+              <div className={styles.dateRow}>
+                <Label label={t('end date')} style={{ flexGrow: 1 }}>
+                  <Input
+                    type="date"
+                    id="enddate"
+                    value={endDate?.toISOString().split('T')[0]}
+                    onChange={(e) => {
+                      const newDate = e.currentTarget.valueAsDate;
+                      if (newDate) {
+                        setEndDate(endOfDay(newDate));
+                      }
+                    }}
+                    disabled={isLoading}
+                  />
                 </Label>
               </div>
             </Collapse>
