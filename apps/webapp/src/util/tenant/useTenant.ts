e792163f324d23585352e5ee71f2a98b0161fd58
Alexis Rinaldoni <arinaldoni@me.com>
2024-08-25 22:03:13 +0200

    Add tenant to serverdata (und use it in useTenant)

--- apps/webapp/src/util/tenant/useTenant.ts
+++ apps/webapp/src/util/tenant/useTenant.ts
@@ -2,17 +2,25 @@ import * as React from 'react';
 import * as Sentry from '@sentry/nextjs';
 import { TenantModel } from 'model';
 import { useQuery } from '@apollo/client';
+import { useServerData } from 'shared/ServerDataContext';
+
 import GetTenantQuery from 'api/query/GetTenantQuery.graphql';
 
 export const useTenant = (): TenantModel => {
+  const { tenant: serverTenant } = useServerData();
   const { data, error } = useQuery<{ tenant: TenantModel }>(GetTenantQuery);
-  if (!data) {
+
+  const tenant = data?.tenant ?? serverTenant;
+
+  if (!tenant) {
     throw error ?? new Error('Tenant could not be retrieved');
   }
+
   React.useEffect(() => {
-    if (data.tenant) {
-      Sentry.setContext('tenant', data.tenant);
+    if (tenant) {
+      Sentry.setContext('tenant', tenant);
     }
-  }, [data.tenant]);
-  return data.tenant;
+  }, [tenant]);
+
+  return tenant;
 };
