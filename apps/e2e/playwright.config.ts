3809867e28903b66efd1c116475fbc7ffd2a5404
Alexis Rinaldoni <arinaldoni@me.com>
2024-12-29 05:18:31 +0100

    Upload playwright-report as artifact

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -23,7 +23,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [['github'], ['html', { outputFolder: 'playwright-report' }]]
+    : [['list']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
