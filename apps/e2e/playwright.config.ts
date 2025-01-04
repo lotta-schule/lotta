87640d94de6bf99c9c76c4b831f53c54e3cb0b4a
Alexis Rinaldoni <arinaldoni@me.com>
2025-01-04 14:33:45 +0100

    Add comments with links o test results

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   testMatch: '**/*.test.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
