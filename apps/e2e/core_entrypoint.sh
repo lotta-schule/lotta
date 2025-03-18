71c4492532fba9a47fe4c96aeab4c2660d08c132
Alexis Rinaldoni <arinaldoni@me.com>
2025-03-18 18:49:57 +0100

    remove entrypoint

--- apps/e2e/core_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# migrate database
-./bin/lotta eval "Application.ensure_all_started(:lotta)" # Do not check in
-./bin/lotta eval "Lotta.Release.migrate()"
-
-# start the server
-./bin/lotta $@
