b2c7f5592101ed80ddcc0115ad9febb0d359b021
Alexis Rinaldoni <arinaldoni@me.com>
2025-08-03 14:46:48 +0200

    chore(core): streamline Tesla clients

--- apps/core-api/config/config/tesla.exs
+++ apps/core-api/config/config/tesla.exs
@@ -8,20 +8,6 @@ adapter =
 
 config :tesla, adapter: adapter
 
-log_level_fn =
-  case config_env() do
-    :dev ->
-      :debug
-
-    _ ->
-      fn
-        %{status: status} when status <= 399 -> :info
-        %{status: status} when status in 400..499 -> :warn
-        %{status: status} when status >= 500 -> :info
-      end
-  end
-
 config :tesla, Tesla.Middleware.Logger,
   filter_headers: ["authorization", "cookie", "set-cookie"],
-  log_level: log_level_fn,
   debug: config_env() == :dev
