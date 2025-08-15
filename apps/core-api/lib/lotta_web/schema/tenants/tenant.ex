16ad7f93e829775f60490ac62b9408d385e212ec
Alexis Rinaldoni <arinaldoni@me.com>
2025-08-15 05:37:16 +0200

    Add state to tenant graphql schema

--- apps/core-api/lib/lotta_web/schema/tenants/tenant.ex
+++ apps/core-api/lib/lotta_web/schema/tenants/tenant.ex
@@ -9,6 +9,7 @@ defmodule LottaWeb.Schema.Tenants.Tenant do
     field(:id, non_null(:id))
     field(:title, non_null(:string))
     field(:type, :string)
+    field(:state, non_null(:tenant_state))
     field(:slug, non_null(:string))
     field(:address, non_null(:string))
     field(:host, non_null(:string), resolve: &TenantResolver.resolve_host/3)
@@ -65,5 +66,11 @@ defmodule LottaWeb.Schema.Tenants.Tenant do
   input_object :tenant_configuration_input do
     field(:custom_theme, :json)
     field(:user_max_storage_config, :string)
+  end
+
+  enum :tenant_state do
+    value(:init, description: "Tenant is being initialized")
+    value(:active, description: "Tenant is active and operational")
+    value(:readonly, description: "Tenant is in read-only mode")
   end
 end
