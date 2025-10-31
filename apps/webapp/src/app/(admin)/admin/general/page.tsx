6fe239f835eea5f3724b9453a3dc657b13e5d696
Alexis Rinaldoni <ptitmouton@users.noreply.github.com>
2025-10-31 18:21:47 +0100

    feat(web): add docs link to admin menu (#507)
    Closes #501

--- apps/webapp/src/app/(admin)/admin/general/page.tsx
+++ apps/webapp/src/app/(admin)/admin/general/page.tsx
@@ -3,12 +3,14 @@ import { GeneralSettings } from './GeneralSettings';
 import { loadTenant } from 'loader';
 import { AdminPage } from 'app/(admin)/admin/_component/AdminPage';
 import { faSliders } from '@fortawesome/free-solid-svg-icons';
+import { serverTranslations } from 'i18n/server';
 
 async function GeneralSettingsPage() {
   const tenant = await loadTenant();
+  const { t } = await serverTranslations();
 
   return (
-    <AdminPage icon={faSliders} title="Grundeinstellungen" hasHomeLink>
+    <AdminPage icon={faSliders} title={t('general')} hasHomeLink>
       <GeneralSettings tenant={tenant} />
     </AdminPage>
   );
