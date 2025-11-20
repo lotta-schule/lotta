fc988162fd53a577d7f20155f56908707df7635b
Alexis Rinaldoni <arinaldoni@me.com>
2025-11-20 19:18:24 +0100

    feat(core): show preview of invoice

--- apps/core-api/lib/lotta_web/live/invoice_live.ex
+++ apps/core-api/lib/lotta_web/live/invoice_live.ex
@@ -1,4 +1,6 @@
 defmodule LottaWeb.Live.InvoiceLive do
+  alias Lotta.Billings.Invoice
+
   use Backpex.LiveResource,
     adapter_config: [
       schema: Lotta.Billings.Invoice,
@@ -63,13 +65,17 @@ defmodule LottaWeb.Live.InvoiceLive do
 
   @impl Backpex.LiveResource
   def render_resource_slot(assigns, :show, :main) do
-    IO.inspect(assigns)
-
     ~H"""
     <iframe
-      src="https://google.com"
-      class="w-full h-min border-0"
+      src={get_html_src(@item)}
+      onLoad="this.style.height=this.contentWindow.document.body.scrollHeight +'px';"
+      style="width: min(80%, 794px); aspect-ratio: 210 / 297; border: none; zoom: .75; padding: 1em;"
     />
     """
+  end
+
+  defp get_html_src(%Invoice{} = invoice) do
+    invoice = Lotta.Repo.preload(invoice, :items)
+    "data:text/html;base64,#{Base.encode64(Invoice.to_html(invoice))}"
   end
 end
